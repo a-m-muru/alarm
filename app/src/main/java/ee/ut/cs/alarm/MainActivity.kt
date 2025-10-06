cb66914d1597a07c7ea71f08349453702cc78fbc
Kauri <kauriremm@gmail.com>
2025-10-06 12:47:12 +0300

    sensor screen

--- app/src/main/java/ee/ut/cs/alarm/MainActivity.kt
+++ app/src/main/java/ee/ut/cs/alarm/MainActivity.kt
@@ -4,13 +4,24 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import ee.ut.cs.alarm.ui.theme.AlarmTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,13 +30,36 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             AlarmTheme {
+                // A state to manage which screen is currently shown
+                var currentScreen by remember { mutableStateOf("main") }
+
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    when (currentScreen) {
+                        "main" -> MainScreen(
+                            modifier = Modifier.padding(innerPadding),
+                            onNavigateToSensors = { currentScreen = "sensors" }
+                        )
+                        "sensors" -> SensorScreen(
+                            onNavigateBack = { currentScreen = "main" }
+                        )
+                    }
                 }
             }
+        }
+    }
+}
+
+@Composable
+fun MainScreen(modifier: Modifier = Modifier, onNavigateToSensors: () -> Unit) {
+    Column(
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Greeting(name = "Android")
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(onClick = onNavigateToSensors) {
+            Text("Show Sensor Info")
         }
     }
 }
