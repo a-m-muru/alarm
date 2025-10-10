63ab6d336ea7c471296f2fb694a4d0aa5e958cd0
amm <aksel.martin.muru@ut.ee>
2025-10-10 10:57:42 +0300

    Remove impords

--- app/src/main/java/ee/ut/cs/alarm/AlarmActivity.kt
+++ app/src/main/java/ee/ut/cs/alarm/AlarmActivity.kt
@@ -18,11 +18,6 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
@@ -31,11 +26,7 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import ee.ut.cs.alarm.gaming.AudioPlayer
-import ee.ut.cs.alarm.gaming.Vec2
 import ee.ut.cs.alarm.ui.theme.AlarmTheme
-import kotlinx.coroutines.delay
-import java.util.ArrayList
-import kotlin.math.sin
 import kotlin.random.Random
 
 class AlarmActivity : ComponentActivity() {
