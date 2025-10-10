eed72b921960c48c21403b538896bd06d7b60fb0
amm <aksel.martin.muru@ut.ee>
2025-10-10 10:52:52 +0300

    minigame screen place where to go go

--- app/src/main/java/ee/ut/cs/alarm/AlarmActivity.kt
+++ app/src/main/java/ee/ut/cs/alarm/AlarmActivity.kt
@@ -1,30 +1,42 @@
 package ee.ut.cs.alarm
 
 import android.content.Context
+import android.media.PlaybackParams
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import ee.ut.cs.alarm.gaming.AudioPlayer
-import ee.ut.cs.alarm.gaming.GameLoob
+import ee.ut.cs.alarm.gaming.Vec2
 import ee.ut.cs.alarm.ui.theme.AlarmTheme
+import kotlinx.coroutines.delay
+import java.util.ArrayList
+import kotlin.math.sin
+import kotlin.random.Random
 
 class AlarmActivity : ComponentActivity() {
 
@@ -61,13 +73,43 @@ class AlarmActivity : ComponentActivity() {
         ) {
             Spacer(modifier = Modifier.height(16.dp))
             Text("You have the alarm!", fontSize = 24.sp)
-            Text(timeString, fontSize = 84.sp)
+            Text(timeString, fontSize = 84.sp, fontWeight = FontWeight.Bold)
             Text(dateString, fontSize = 16.sp)
             Spacer(modifier = Modifier.height(16.dp))
             Button(
-                onClick = { AudioPlayer.playSound(ctx, R.raw.bump) }
+                onClick = {
+                    AudioPlayer.playSound(
+                        ctx,
+                        R.raw.bump,
+                        PlaybackParams().setPitch(Random.nextFloat() + 1.0f)
+                    )
+                }
             ) { Text("Sneeze 7 min") }
+            Column(
+                modifier = Modifier
+                    .padding(8.dp)
+                    .fillMaxSize()
+                    .background(
+                        Brush.sweepGradient(
+                            listOf(
+                                Color.Magenta,
+                                Color.Blue,
+                                Color.Black,
+                                Color.Blue,
+                                Color.Magenta
+                            )
+                        )
+                    ),
+                verticalArrangement = Arrangement.Center
+            ) {
+                minigameScreen()
+            }
         }
+    }
+
+    @Composable
+    fun minigameScreen() {
+
     }
 
 }
