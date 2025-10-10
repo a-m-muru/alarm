4ee4a6a3de173fb0614845ee02fd776bd65da3e2
amm <aksel.martin.muru@ut.ee>
2025-10-10 10:52:52 +0300

    minigame screen place where to go go

--- app/src/main/java/ee/ut/cs/alarm/gaming/AudioPlayer.kt
+++ app/src/main/java/ee/ut/cs/alarm/gaming/AudioPlayer.kt
@@ -2,11 +2,13 @@ package ee.ut.cs.alarm.gaming
 
 import android.content.Context
 import android.media.MediaPlayer
+import android.media.PlaybackParams
 
 object AudioPlayer {
-    fun playSound(ctx: Context, soundId: Int) {
+    fun playSound(ctx: Context, soundId: Int, params: PlaybackParams = PlaybackParams()) {
         val mp = MediaPlayer.create(ctx, soundId)
         mp.setOnCompletionListener { mp -> mp.release() }
+        mp.playbackParams = params
         mp.start()
     }
 }
