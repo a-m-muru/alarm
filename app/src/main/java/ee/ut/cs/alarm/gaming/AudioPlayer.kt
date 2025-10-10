a247c94c1a1da0fbee048edb7d90211f0b789f07
amm <aksel.martin.muru@ut.ee>
2025-10-10 10:02:15 +0300

    Fix my warnings

--- app/src/main/java/ee/ut/cs/alarm/gaming/AudioPlayer.kt
+++ app/src/main/java/ee/ut/cs/alarm/gaming/AudioPlayer.kt
@@ -2,7 +2,6 @@ package ee.ut.cs.alarm.gaming
 
 import android.content.Context
 import android.media.MediaPlayer
-import ee.ut.cs.alarm.R
 
 object AudioPlayer {
     fun playSound(ctx: Context, soundId: Int) {
