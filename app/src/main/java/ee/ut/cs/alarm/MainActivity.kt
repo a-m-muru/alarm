1c2f952c49a467fef4749e950bbd5d3b76689c33
inugami-dev64 <karmen@nyaa.ee>
2025-10-14 11:07:27 +0300

    Create a placeholder AlarmListScreen screen

--- app/src/main/java/ee/ut/cs/alarm/MainActivity.kt
+++ app/src/main/java/ee/ut/cs/alarm/MainActivity.kt
@@ -4,26 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import ee.ut.cs.alarm.alarming.AlarmScheduler
-import ee.ut.cs.alarm.data.Alarm
+import ee.ut.cs.alarm.ui.screens.AlarmListScreen
 import ee.ut.cs.alarm.ui.theme.AlarmTheme
 
 class MainActivity : ComponentActivity() {
@@ -37,58 +26,9 @@ class MainActivity : ComponentActivity() {
                 var currentScreen by remember { mutableStateOf("main") }
 
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    when (currentScreen) {
-                        "main" -> MainScreen(
-                            modifier = Modifier.padding(innerPadding),
-                            onNavigateToSensors = { currentScreen = "sensors" },
-                            createAlarm = { alarmScheduler.scheduleAlarm(Alarm(1u, 2)) }
-                        )
-
-                        "sensors" -> SensorScreen(
-                            onNavigateBack = { currentScreen = "main" }
-                        )
-                    }
+                    AlarmListScreen(innerPadding)
                 }
             }
         }
-    }
-}
-
-@Composable
-fun MainScreen(
-    modifier: Modifier = Modifier,
-    onNavigateToSensors: () -> Unit,
-    createAlarm: () -> Unit
-) {
-    Column(
-        modifier = modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Greeting(name = "Android")
-        Spacer(modifier = Modifier.height(16.dp))
-        Button(onClick = onNavigateToSensors) {
-            Text("Show Sensor Info")
-        }
-        Spacer(modifier = Modifier.height(16.dp))
-        Button(onClick = createAlarm) {
-            Text("Give me Alarm....")
-        }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    AlarmTheme {
-        Greeting("Android")
     }
 }
---
+++ app/src/main/java/ee/ut/cs/alarm/ui/screens/AlarmListScreen.kt
@@ -0,0 +1,51 @@
+package ee.ut.cs.alarm.ui.screens
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun AlarmListScreen(innerPadding: PaddingValues) {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(innerPadding)
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp)
+        ) {
+            Text(
+                text = "My Alarms",
+                fontSize = 32.sp,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.padding(bottom = 16.dp)
+            )
+
+            Box(
+                modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(
+                    text = "No alarms set\nTap + to add your first alarm",
+                    textAlign = TextAlign.Center,
+                    color = MaterialTheme.colorScheme.onSurfaceVariant
+                )
+            }
+        }
+    }
+}
