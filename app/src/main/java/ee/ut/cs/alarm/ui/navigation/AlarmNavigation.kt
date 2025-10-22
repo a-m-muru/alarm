a3d40821e192e0cf3b7f951850da168eebb378ed
inugami-dev64 <karmen@nyaa.ee>
2025-10-22 11:19:39 +0300

    Modify AlarmNavigation so that the list viewmodel would get passed to the list screen

--- app/src/main/java/ee/ut/cs/alarm/ui/navigation/AlarmNavigation.kt
+++ app/src/main/java/ee/ut/cs/alarm/ui/navigation/AlarmNavigation.kt
@@ -1,11 +1,15 @@
 package ee.ut.cs.alarm.ui.navigation
 
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import ee.ut.cs.alarm.ui.screens.AboutScreen
 import ee.ut.cs.alarm.ui.screens.AlarmListScreen
+import ee.ut.cs.alarm.ui.viewmodel.AlarmListViewModel
+import ee.ut.cs.alarm.ui.viewmodel.AlarmListViewModelFactory
 
 @Composable
 fun AlarmNavigation(
@@ -23,12 +27,14 @@ fun AlarmNavigation(
         }
     }
 
+    val vm: AlarmListViewModel = viewModel(factory = AlarmListViewModelFactory(LocalContext.current.packageName))
+
     NavHost(
         navController = navController,
         startDestination = Screen.AlarmList.route
     ) {
         composable(Screen.AlarmList.route) {
-            AlarmListScreen(onNavigate)
+            AlarmListScreen(onNavigate, vm)
         }
         composable(Screen.About.route) {
             AboutScreen(onNavigate)
