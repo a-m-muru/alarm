87d136a29d79843a6b7aa98315a4b22284a1b8c4
amm <aksel.martin.muru@ut.ee>
2025-10-28 19:04:33 +0200

    Add weather showcase to alarm sreen

--- app/src/main/java/ee/ut/cs/alarm/MainActivity.kt
+++ app/src/main/java/ee/ut/cs/alarm/MainActivity.kt
@@ -1,19 +1,12 @@
 package ee.ut.cs.alarm
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.navigation.compose.rememberNavController
-import ee.ut.cs.alarm.data.Weather
 import ee.ut.cs.alarm.ui.navigation.AlarmNavigation
 import ee.ut.cs.alarm.ui.theme.AlarmTheme
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,17 +16,6 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             AlarmTheme {
-
-                val coroutineScope = rememberCoroutineScope()
-
-                LaunchedEffect(Unit) {
-                    coroutineScope.launch {
-                        val weather = withContext(Dispatchers.IO) {
-                            Weather.fromRequest()
-                        }
-                        Log.d("lol", weather.toString())
-                    }
-                }
 
                 val navController = rememberNavController()
 
