7556a2f9f12d1b7e80c761235202e7a4f54dea2a
inugami-dev64 <karmen@nyaa.ee>
2025-11-06 10:30:40 +0200

    Modify Weather data class to make it XML deserializable and create Observations data class

--- app/src/main/java/ee/ut/cs/alarm/data/Weather.kt
+++ app/src/main/java/ee/ut/cs/alarm/data/Weather.kt
@@ -1,132 +1,64 @@
 package ee.ut.cs.alarm.data
 
-import android.util.Log
-import org.xmlpull.v1.XmlPullParserFactory
-import java.io.StringReader
-import java.net.URL
-import org.xmlpull.v1.XmlPullParser as xpp
+import org.simpleframework.xml.Element
+import org.simpleframework.xml.ElementList
+import org.simpleframework.xml.Root
 
-val logTag = "WEATHER PARSER"
+@Root(strict = false, name = "observations")
+data class Observations(
+    @field:ElementList(inline = true, entry = "station")
+    var stations: List<Weather> = mutableListOf()
+)
 
-data class Weather(val temperature: Float, val phenomenon: String, val windspeed: Float) {
-    companion object {
+@Root(name = "station", strict = false)
+data class Weather(
+    @field:Element(name = "name",  required = false)
+    var name: String = "",
 
-        private fun getTagInfo(parser: xpp, tag: String): String {
-            val evt = parser.next()
-            if (evt != xpp.TEXT) {
-                Log.e(logTag, "didn't find $tag in measurements")
-                return "-1111"
-            } else {
-                return parser.text
-            }
-        }
+    @field:Element(name = "wmocode",  required = false)
+    var wmoCode: Int? = null,
 
-        fun fromRequest(): Weather {
-            val TAG_OBSERVATIONS = "observations"
-            val TAG_STATION = "station"
-            val TAG_NAME = "name"
-            val TAG_PHENOMENON = "phenomenon"
-            val TAG_TEMPERATURE = "airtemperature"
-            val TAG_WINDSPEED = "windspeed"
+    @field:Element(name = "longitude",  required = false)
+    var longitude: Double = 0.0,
 
-            val NAME_STATION = "Tartu-Tõravere" // we look at this station for Tartu weather
-            // why don't we look at any other weather?
-            // because only weather in Tartu is important
+    @field:Element(name = "latitude",  required = false)
+    var latitude: Double = 0.0,
+    @field:Element(name = "phenomenon",  required = false)
+    var phenomenon: String? = null,
+    @field:Element(name = "visibility",  required = false)
+    var visibility: Float? = null,
+    @field:Element(name = "precipitations",  required = false)
+    var precipitations: Float? = null,
+    @field:Element(name = "airpressure",  required = false)
+    var airPressure: Float? = null,
 
-            val S_WRONG = -1
-            val S_SEARCHING = 1
-            val S_SEARCHING_STATION = 2
-            val S_IN_STATION = 3
+    @field:Element(name = "relativehumidity",  required = false)
+    var relativeHumidity: Float? = null,
 
-            var state = S_WRONG
+    @field:Element(name = "airtemperature",  required = false)
+    var airTemperature: Float? = null,
 
-            // using basic Java URL. simple URL GET request, no parameters, no issue
-            var txt: String = ""
-            val url = URL("https://www.ilmateenistus.ee/ilma_andmed/xml/observations.php")
-            try {
-                txt = url.readText()
-            } catch (e: Exception) {
-            }
-            Log.d(logTag, "length of gotten text is ${txt.length}")
+    @field:Element(name = "winddirection",  required = false)
+    var windDirection: Float? = null,
 
-            // use basic simple xml parsing that is provided with android
-            // API is simple and only one thing is needed from it so this is optimal
-            val parser = XmlPullParserFactory.newInstance().newPullParser()
-            parser.setInput(StringReader(txt))
+    @field:Element(name = "windspeed",  required = false)
+    var windSpeed: Float? = null,
 
-            // these values will be stored. -1111 is used as an arbitrary "unset" value
-            var temperature: Float = -1111f
-            var phenomenon: String = "-1111"
-            var windspeed: Float = -1111f
+    @field:Element(name = "windspeedmax",  required = false)
+    var windSpeedMax: Float? = null,
 
-            var successes = 0
+    @field:Element(name = "waterlevel_eh2000",  required = false)
+    var waterLevelEH2000: Float? = null,
 
-            // xml tree format:
-            //   observations         // root element
-            //     station            // API lists multiple weather stations, we pick one
-            //       name             // name of the station
-            //       temperature      // in degrees celsius
-            //       windspeed        // in meters/second
-            //       phenomenon       // English description of weather
-            var eventType = parser.eventType
-            while (eventType != xpp.END_DOCUMENT) {
+    @field:Element(name = "watertemperature",  required = false)
+    var waterTemperature: Float? = null,
 
-                // trying to find the station we're interested in
-                if (state == S_WRONG && eventType == xpp.START_TAG && parser.name == TAG_OBSERVATIONS) {
-                    Log.d(logTag, "started searching for station")
-                    state = S_SEARCHING
-                } else if (state == S_SEARCHING && eventType == xpp.START_TAG && parser.name == TAG_STATION) {
-                    Log.d(logTag, "started searching in station")
-                    state = S_SEARCHING_STATION
-                } else if (state == S_SEARCHING_STATION && eventType == xpp.START_TAG && parser.name == TAG_NAME) {
-                    val evt = parser.next()
-                    if (evt != xpp.TEXT) {
-                        // name was empty? skip
-                        eventType = parser.next()
-                        continue
-                    }
-                    if (parser.text == NAME_STATION) {
-                        // found correct station, set state
-                        Log.d(logTag, "found correct station")
-                        state = S_IN_STATION
-                    }
-                }
+    @field:Element(name = "uvindex",  required = false)
+    var uvIndex: Float? = null,
 
-                // looking for data in the correct station
-                else if (state == S_IN_STATION) {
-                    if (eventType == xpp.END_TAG && parser.name == TAG_STATION) {
-                        // done parsing the intended station, end
-                        state = S_WRONG
-                        break
-                    } else if (eventType == xpp.START_TAG) {
-                        // upon finding the parameters we're looking for, store them
-                        if (parser.name == TAG_TEMPERATURE) {
-                            temperature = getTagInfo(parser, TAG_TEMPERATURE).toFloat()
-                        } else if (parser.name == TAG_PHENOMENON) {
-                            phenomenon = getTagInfo(parser, TAG_PHENOMENON)
-                        } else if (parser.name == TAG_WINDSPEED) {
-                            windspeed = getTagInfo(parser, TAG_WINDSPEED).toFloat()
-                        }
-                    }
-                }
+    @field:Element(name = "sunshineduration",  required = false)
+    var sunshineDuration: Int? = null,
 
-                eventType = parser.next()
-            }
-
-            return Weather(temperature, phenomenon, windspeed)
-
-        }
-    }
-
-    fun hasError(): Boolean {
-        return temperature == -1111F || phenomenon == "-1111" || windspeed == -1111F
-    }
-
-    fun getString(): String {
-        // if we didn't get weather data, don't display it.
-        // if the API integration was more important, we'd notify the user.
-        // waking up and weather aren't that importantly correlated so we can do this.
-        if (hasError()) return ""
-        return "$temperature°C | Wind $windspeed m/s | $phenomenon"
-    }
-}
+    @field:Element(name = "globalradiation",  required = false)
+    var globalRadiation: Float? = null
+)
