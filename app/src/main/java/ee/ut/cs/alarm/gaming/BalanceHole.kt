d4789512ea32c68164eef08f96d25e3dc55fc4a3
Kauri <kauriremm@gmail.com>
2025-11-18 13:26:11 +0200

    Hole game is playable

--- app/src/main/java/ee/ut/cs/alarm/gaming/BalanceHole.kt
+++ app/src/main/java/ee/ut/cs/alarm/gaming/BalanceHole.kt
@@ -7,11 +7,15 @@ import android.hardware.SensorEventListener
 import android.hardware.SensorManager
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.LaunchedEffect
@@ -30,6 +34,7 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import kotlin.math.round
 import kotlin.math.roundToInt
 import kotlin.math.sign
@@ -50,9 +55,12 @@ class GameEngine {
     var screenHeight by mutableStateOf(0.dp)
 
     var balls = mutableStateListOf<Ball>()
+    var holes = mutableStateListOf<Hole>()
+
 
     fun startGame() {
         balls.clear()
+        holes.clear()
 
         repeat(8) {
             var ball =
@@ -64,21 +72,89 @@ class GameEngine {
 
             balls.add(ball)
         }
+
+        var biggestBallRadious = 0f
+        for (ball in balls) {
+            if (ball.radius > biggestBallRadious) {
+                biggestBallRadious = ball.radius
+            }
+        }
+
+
+        val hole =
+            Hole(
+                pos = Vec2(0f, 0f),
+                radius = biggestBallRadious * 1.2f
+            )
+        hole.pos = Vec2(
+            Random.nextFloat() *
+                    (screenWidth.value - 2 * hole.radius) + hole.radius,
+            Random.nextFloat() *
+                    (screenHeight.value - 2 * hole.radius) + hole.radius
+        )
+
+        holes.add(hole)
+
+
+        repeat(3){
+            val hole =
+                Hole(
+                    pos = Vec2(0f, 0f),
+                    radius = Random.nextFloat() * biggestBallRadious + 15f,
+                )
+            hole.pos = Vec2(
+                Random.nextFloat() *
+                        (screenWidth.value-2*hole.radius) + hole.radius,
+                Random.nextFloat() *
+                        (screenHeight.value-2*hole.radius) + hole.radius
+            )
+
+
+            holes.add(hole)
+        }
+
     }
 
     fun update(time: Long) {
         val delta = time - prevTime
-        val deltaT = (delta / 1E8).toFloat()
+        val deltaT = (delta / 1E9).toFloat()
         totalTime += delta
         prevTime = time
 
         if (frames > 0L) {
-            for (ball in balls) ball.update(deltaT, this)
+            for (ball in balls) {
+                ball.update(deltaT, this)
+
+                // check ball-hole collision
+                // Iterate over a copy of the list to avoid ConcurrentModificationException
+                for (hole in holes.toList()) {
+                    val distance = ball.pos.distanceTo(hole.pos)
+
+                    // Check if the ball is inside the hole
+                    if (distance < (hole.radius - ball.radius)) {
+                        if (ball.vel.length() < 20000f) {
+                            balls.remove(ball)
+                            return
+                        }
+                    }
+                }
+            }
         }
+
+
+
         frames += 1
     }
 
     override fun toString(): String = "balls: " + balls + "; time: ${(totalTime / 1E8).roundToInt() / 10f}, orientation: $rotation"
+}
+
+class Hole(
+    pos: Vec2,
+    radius: Float,
+) {
+    var pos by mutableStateOf(pos)
+    var radius by mutableStateOf(radius)
 }
 
 class Ball(
@@ -87,6 +163,10 @@ class Ball(
 ) {
     val BOUNCE_ENERGY_MULT = 0.9f
     val FRICTION = 0.2f
+
+    val ACC_MULT = 100f
+
+    val MAX_VEL = 10000f
 
     var vel by mutableStateOf(Vec2(30f, 70f))
     var pos by mutableStateOf(pos)
@@ -98,26 +178,39 @@ class Ball(
     ) {
         // Log.i("ball", "position: " + pos.x + " " + pos.y)
         // Log.i("ball", "delta: " + deltaT)
+
+        //todo if z is negative, go crazy
+
+
+        vel.x += engine.rotation.x * deltaT * ACC_MULT
         pos.x += vel.x * deltaT
-        vel.x += engine.rotation.x * deltaT * 10f
-        vel.y += engine.rotation.y * deltaT * 10f
+
+        // Wall collision x
+        // Right wall
         if ((pos.x + radius).dp > engine.screenWidth) {
             pos.x = engine.screenWidth.value - radius
             vel.x *= -BOUNCE_ENERGY_MULT
         }
+        // Left wall
         if ((pos.x - radius).dp < 0.dp) {
             pos.x = 0.dp.value + radius
             vel.x *= -BOUNCE_ENERGY_MULT
         }
+
+        // Wall collision y
+        // Bottom wall
+        vel.y += engine.rotation.y * deltaT * ACC_MULT
         pos.y += vel.y * deltaT
         if ((pos.y + radius).dp > engine.screenHeight) {
             pos.y = engine.screenHeight.value - radius
             vel.y *= -BOUNCE_ENERGY_MULT
         }
+        // Top wall
         if ((pos.y - radius).dp < 0.dp) {
             pos.y = 0.dp.value + radius
             vel.y *= -BOUNCE_ENERGY_MULT
         }
+
         pos = Vec2(pos.x, pos.y)
         vel.x -= deltaT * FRICTION * sign(vel.x)
         vel.y -= deltaT * FRICTION * sign(vel.y)
@@ -132,7 +225,7 @@ val Ball.yOffset: Dp get() = pos.y.dp - radius.dp
 @Composable
 fun BalanceHole(onNavigateBack: () -> Unit) {
     val gameEngine = remember { GameEngine() }
-    gameEngine.startGame()
+    //gameEngine.startGame()
 
     val density = LocalDensity.current
     val context = LocalContext.current
@@ -176,7 +269,9 @@ fun BalanceHole(onNavigateBack: () -> Unit) {
             sensorManager.unregisterListener(sensorEventListener)
         }
     }
-
+    LaunchedEffect(Unit) {
+        gameEngine.startGame()
+    }
     LaunchedEffect(Unit) {
         while (true) {
             withFrameNanos {
@@ -199,7 +294,17 @@ fun BalanceHole(onNavigateBack: () -> Unit) {
                 },
     ) {
         // Text("" + gameEngine)
+        gameEngine.holes.forEach { DrawHole(it) }
         gameEngine.balls.forEach { DrawBall(it) }
+
+
+        if (gameEngine.balls.isEmpty()) {
+            Text(text = "Congratulations! You did it!", fontSize = 24.sp)
+            Spacer(modifier = Modifier.height(24.dp))
+            Button(onClick = onNavigateBack) {
+                Text("Go Back")
+            }
+        }
     }
 }
 
@@ -212,5 +317,17 @@ fun DrawBall(ball: Ball) {
             .size(size * 2)
             .clip(CircleShape)
             .background(Color.Red),
+    )
+}
+
+@Composable
+fun DrawHole(hole: Hole){
+    val size = hole.radius.dp
+    Box(
+        Modifier
+            .offset(hole.pos.x.dp - size, hole.pos.y.dp - size)
+            .size(size * 2)
+            .clip(CircleShape)
+            .background(Color.Black),
     )
 }
