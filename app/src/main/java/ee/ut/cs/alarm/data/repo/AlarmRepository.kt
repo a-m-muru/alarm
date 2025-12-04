c2d7980d981cbd6beb73ce0795a2c539a97acd7a
amm <aksel.martin.muru@ut.ee>
2025-12-04 11:55:15 +0200

    streak in proto

--- app/src/main/java/ee/ut/cs/alarm/data/repo/AlarmRepository.kt
+++ app/src/main/java/ee/ut/cs/alarm/data/repo/AlarmRepository.kt
@@ -5,9 +5,10 @@ import androidx.datastore.core.DataStore
 import androidx.datastore.core.Serializer
 import androidx.datastore.dataStore
 import ee.ut.cs.alarm.data.Alarm
-import ee.ut.cs.alarm.data.proto.AlarmListProto;
+import ee.ut.cs.alarm.data.proto.AlarmListProto
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.map
 import java.io.InputStream
 import java.io.OutputStream
@@ -16,51 +17,58 @@ import java.util.UUID
 object AlarmListSerializer : Serializer<AlarmListProto> {
     override val defaultValue: AlarmListProto = AlarmListProto.getDefaultInstance()
 
-    override suspend fun readFrom(input: InputStream): AlarmListProto {
-        return AlarmListProto.parseFrom(input)
-    }
+    override suspend fun readFrom(input: InputStream): AlarmListProto = AlarmListProto.parseFrom(input)
 
     override suspend fun writeTo(
         t: AlarmListProto,
-        output: OutputStream
+        output: OutputStream,
     ) {
         t.writeTo(output)
     }
-
 }
 
 interface AlarmRepository {
     suspend fun getAlarms(): Flow<List<Alarm>>
+
     suspend fun saveAlarm(alarm: Alarm)
-    suspend fun deleteAlarm(alarm: Alarm);
-    suspend fun getAlarmByID(alarmID: UUID): Alarm?;
+
+    suspend fun deleteAlarm(alarm: Alarm)
+
+    suspend fun getAlarmByID(alarmID: UUID): Alarm?
+
+    suspend fun getStreak(): Int
+
+    suspend fun setStreak(to: Int)
 }
 
-class AlarmRepositoryImpl private constructor(private val context: Context) : AlarmRepository {
+class AlarmRepositoryImpl private constructor(
+    private val context: Context,
+) : AlarmRepository {
     private val Context.alarmDataStore: DataStore<AlarmListProto> by dataStore(
         fileName = "alarms.pb",
-        serializer = AlarmListSerializer
+        serializer = AlarmListSerializer,
     )
 
-    override suspend fun getAlarms(): Flow<List<Alarm>> {
-        return context.alarmDataStore.data
+    override suspend fun getAlarms(): Flow<List<Alarm>> =
+        context.alarmDataStore.data
             .catch { exception -> throw exception }
             .map { alarmList ->
                 alarmList.alarmsList.map { Alarm.fromProto(it) }
             }
-    }
 
     override suspend fun saveAlarm(alarm: Alarm) {
         context.alarmDataStore.updateData { currentList ->
             val existingAlarms = currentList.alarmsList.toMutableList()
             val existingIndex = existingAlarms.indexOfFirst { it.id == alarm.id.toString() }
 
-            if (existingIndex >= 0)
+            if (existingIndex >= 0) {
                 existingAlarms[existingIndex] = alarm.toProto()
-            else
+            } else {
                 existingAlarms.add(alarm.toProto())
+            }
 
-            currentList.toBuilder()
+            currentList
+                .toBuilder()
                 .clearAlarms()
                 .addAllAlarms(existingAlarms)
                 .build()
@@ -69,31 +77,49 @@ class AlarmRepositoryImpl private constructor(private val context: Context) : Al
 
     override suspend fun deleteAlarm(alarm: Alarm) {
         context.alarmDataStore.updateData { currentList ->
-            currentList.toBuilder()
+            currentList
+                .toBuilder()
                 .clearAlarms()
                 .addAllAlarms(
-                    currentList.alarmsList.filter { it.id != alarm.id.toString() }
-                )
-                .build()
+                    currentList.alarmsList.filter { it.id != alarm.id.toString() },
+                ).build()
         }
     }
 
-    override suspend fun getAlarmByID(alarmID: UUID): Alarm? {
-        return context.alarmDataStore.data.map { alarmList ->
-            alarmList.alarmsList.find { it.id == alarmID.toString() }?.let { Alarm.fromProto(it) }
-        }.catch { null }.let {
-            null
+    override suspend fun getAlarmByID(alarmID: UUID): Alarm? =
+        context.alarmDataStore.data
+            .map { alarmList ->
+                alarmList.alarmsList
+                    .find { it.id == alarmID.toString() }
+                    ?.let { Alarm.fromProto(it) }
+            }.catch { null }
+            .let {
+                null
+            }
+
+    override suspend fun getStreak(): Int =
+        context.alarmDataStore.data
+            .map { it.streak }
+            .first()
+
+    override suspend fun setStreak(to: Int) {
+        context.alarmDataStore.updateData { currentList ->
+            currentList
+                .toBuilder()
+                .setStreak(to)
+                .build()
         }
     }
 
     companion object {
         @Volatile
-        private var INSTANCE: AlarmRepository? = null;
+        private var singletonInstance: AlarmRepository? = null
 
-        fun getInstance(context: Context): AlarmRepository {
-            return INSTANCE ?: synchronized(this) {
-                INSTANCE ?: AlarmRepositoryImpl(context.applicationContext).also { INSTANCE = it }
+        fun getInstance(context: Context): AlarmRepository =
+            singletonInstance ?: synchronized(this) {
+                singletonInstance ?: AlarmRepositoryImpl(context.applicationContext).also {
+                    singletonInstance = it
+                }
             }
-        }
     }
 }
